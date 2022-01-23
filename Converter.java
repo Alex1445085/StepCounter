895e7fbb55ff34dcd478f61a3616387ba67b6979
Алексей <n1445085@yandex.ru>
2022-01-23 11:43:25 +0300

    ..пятый раз я закинул невод.. Внесены изменения по замечаниям

--- Converter.java
+++ Converter.java
@@ -2,20 +2,21 @@ public class Converter {
 
     int smPerStep = 75;    // сантиметров в шаге
     int caloPerStep = 50;  // - калорий в шаге
-    double res;
 
     Converter() {
     }
 
     double calculateDist(int allSteps) {
+
         allSteps = allSteps * smPerStep / 1000;  // пройдено шагов * 100
-        res = (double) allSteps / 100;
+        double res = (double) allSteps / 100;
         return res;
     }
 
     double calculateKalo(int allSteps) {
-        allSteps = allSteps * caloPerStep / 100; // сожжено килокалорий * 10
-        res = (double) allSteps / 10;
+
+        allSteps = allSteps * caloPerStep / 100; // сожжено килокалорий * 10  res;
+        double res = (double) allSteps / 10;
         return res;
     }
 }
